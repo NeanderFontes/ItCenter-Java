9f4875fe810b7b964300072e3eba96be0eda7c10
Neander Fontes <neanderffontes@gmail.com>
2023-08-08 20:23:04 +0100

    Nova Branch
    Criando nova branch Teste_metodos para criar um novo projeto utilizando os metodos especiais: get, set, construct.
    Colocando em pratica os métodos de visibilidade utilizando todo o código através de uma instância nova no metodo main principal.

---
+++ ItCenter-Cesae/ficha-Java/src/Poo_estudonauta/Teste_metodos/Alunos.java
@@ -0,0 +1,18 @@
+package Poo_estudonauta.Teste_metodos;
+
+
+/*
+ * Projeto utilizando as estruturas com metodos especiais
+ * Metodos get, set e construct.
+ * Metodos Visibilidade: public, private, protected.
+ * Objeto Aluno.
+ * Atributos: nome, idadeAluno, nota1Aluno, nota2Aluno, mediaAluno, alunoFormado;
+ * Metods:
+ * Estado: status();
+ */
+public class Alunos {
+    private String nome;
+    private int idadeAluno;
+    private double nota1Aluno, nota2Aluno, mediaAluno;
+    private boolean alunoFormado;
+}
