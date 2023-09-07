8eb4c3df4388e29669a50d38898f946884f83828
Neander Fontes <neanderffontes@gmail.com>
2023-09-07 09:57:01 +0100

    Update Tratamento_de_excecoes.java
    Atualizando dados de informação sobre tratamento de exceções.

--- ItCenter-Cesae/ficha-Java/src/Tratamento_de_excecoes.java
+++ ItCenter-Cesae/ficha-Java/src/Tratamento_de_excecoes.java
@@ -1,2 +1,20 @@
+/**
+     O tratamento de exceções em Java é uma maneira de lidar com situações inesperadas ou erros que
+     podem ocorrer durante a execução de um programa.
+     Ele permite que você detecte e lide com esses problemas de forma controlada,
+     em vez de deixar o programa quebrar abruptamente.
+     Em resumo, o tratamento de exceções ajuda a manter a estabilidade do programa,
+     identificando e tratando erros de maneira apropriada. Isso é feito usando blocos try-catch,
+     onde você tenta executar um código (try) e,
+    se ocorrer um erro, você captura (catch) e lida com o erro de uma forma específica.
+ *
+ */
 public class Tratamento_de_excecoes {
+    //Função principal:
+    public static void main(String[] args) {
+        //Entrada de variáveis:
+        int numero[] = new int[4];
+        System.out.println();
+
+    }
 }
