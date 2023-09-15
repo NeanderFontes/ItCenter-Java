06e19f9b68196d9fcc860ede68f7cce11ac0b3b5
Neander Fontes <neanderffontes@gmail.com>
2023-09-15 16:28:25 +0100

    Create Main.java
    Main para instanciar classe livro

---
+++ ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_07/Main.java
@@ -0,0 +1,12 @@
+package Ex_07;
+
+public class Main {
+    /**
+     * Função principal para Livro
+     * @param args
+     */
+    public static void main(String[] args) {
+        //Instancia do objeto Livro:
+        Livro livroTipo1 = new Livro("Harry Potter", "J.K. Rowling", "Fandom", 260, "9789722325332");
+    }
+}
