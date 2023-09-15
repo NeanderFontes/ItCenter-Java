a3cabe947827451c1e1af49582be0a1589cfa3b8
Neander Fontes <neanderffontes@gmail.com>
2023-09-15 14:20:26 +0100

    Create Main.java
    Criando função principal para instanciar objeto Retângulo

---
+++ ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_03/Main.java
@@ -0,0 +1,15 @@
+package Ex_03;
+
+public class Main {
+    /**
+     * Método principal da funão
+     * @param args
+     */
+    public static void main(String[] args) {
+        //Instancia do objeto Retangulo:
+        Retangulo tipo1 = new Retangulo(5, 10);
+
+        //Saída de dados para identificar o objeto criado:
+        System.out.println("Classificando:");
+    }
+}
