ef5eeb27686e27a1dcee4813d04e61a841a84ade
Neander Fontes <neanderffontes@gmail.com>
2023-09-15 14:03:49 +0100

    Create Ex_02 em Poo
    Criando "Ex_02.java" em POO para prática utilizando método especial setter para modificar o valor adicionado na função main sem alterar o valor do atributo "original"

--- ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_02/Cao.java
+++ ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_02/Cao.java
@@ -1,4 +1,27 @@
 package Ex_02;
 
 public class Cao {
+    private String nome, raca, latido = "au au au";
+
+    /**
+     * Método Construtor
+     * @param nome - Nome do cao
+     * @param raca - Raça do cao
+     */
+    public Cao(String nome, String raca) {
+        this.nome = nome;
+        this.raca = raca;
+    }
+
+    /**
+     *
+     * @param latido
+     */
+    public void setLatido(String latido) {
+        this.latido = latido;
+    }
+
+    public void latir() {
+        System.out.println(this.latido);
+    }
 }
--- ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_02/Main.java
+++ ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_02/Main.java
@@ -1,4 +1,24 @@
 package Ex_02;
 
 public class Main {
+    /**
+     * Método principal
+     * @param args
+     */
+    public static void main(String[] args) {
+        //Instancia de classe Cao:
+        Cao bali = new Cao("Bali","Labrador");
+        Cao marrie = new Cao("Marrie", "");
+        Cao caramelo = new Cao("Caramelo", "Vira-Lata");
+
+        //Invocação de metodos para funcionalidade:
+        bali.latir();
+
+        //Chamada para mudar valor através do setter:
+        bali.setLatido("rouf!");
+        bali.latir();
+
+        //Chamada do metodo criado para "latir":
+        marrie.latir();
+    }
 }
