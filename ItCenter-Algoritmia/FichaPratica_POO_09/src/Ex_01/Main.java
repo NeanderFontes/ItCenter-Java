bda78cdd17730589db6b8bad7af0cb33d8ecd7b3
Neander Fontes <neanderffontes@gmail.com>
2023-09-15 13:12:35 +0100

    Commit
    Criando exercicios para treinar instancia e criação de classes, passando referencias em metodos especiais get e set

--- ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_01/Main.java
+++ ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_01/Main.java
@@ -1,4 +1,23 @@
 package Ex_01;
 
 public class Main {
+    /**
+     * Função principal:
+     * @param args
+     */
+    public static void main(String[] args) {
+        //Instancia da Pessoa:
+        Pessoa pessoa1 = new Pessoa("Neander", 34, 1.82);
+        Pessoa pessoa2 = new Pessoa("Rebeca", 35, 1.50);
+
+        //Imprimir no console dados da pessoa1:
+        System.out.println("Nome: " + pessoa1.getNome());
+        System.out.println("Idade: " + pessoa1.getIdade() + "anos");
+        System.out.println("Altura: " + pessoa1.getAltura() + "m");
+
+        //Imprimir no console dados da pessoa2:
+        System.out.println("Nome: " + pessoa2.getNome());
+        System.out.println("Idade: " + pessoa2.getIdade() + "anos");
+        System.out.println("Altura: " + pessoa2.getAltura() + "m");
+    }
 }
--- ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_01/Pessoa.java
+++ ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_01/Pessoa.java
@@ -1,4 +1,38 @@
 package Ex_01;
 
+/**
+ * Classe Pessoa
+ */
 public class Pessoa {
+    private String nome;
+    private int idade;
+    private double altura;
+
+    /**
+     * Método Construtor
+     * @param nome Nome da Pessoa
+     * @param idade Idade Atual
+     * @param altura Altura em metros
+     */
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    /**
+     * Métodos especiais Getter
+     * @return Nome, Idade, Altura
+     */
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public double getAltura() {
+        return this.altura;
+    }
 }
