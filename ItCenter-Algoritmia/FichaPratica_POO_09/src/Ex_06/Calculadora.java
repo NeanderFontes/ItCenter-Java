4ed54698d7cec5b6caf82ae4b3d31ae7d823c3c7
Neander Fontes <neanderffontes@gmail.com>
2023-09-15 15:44:32 +0100

    Create Calculadora.java
    Criando classe calculadora em java, com funções de acordo com seus valores e metodo construtor para instancia do objeto com num1 e num2

---
+++ ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_06/Calculadora.java
@@ -0,0 +1,47 @@
+package Ex_06;
+
+public class Calculadora {
+    private double num1, num2;
+
+    /**
+     * Método Construtor para Entrada de valores para calcular
+     * @param num1
+     * @param num2
+     */
+    public Calculadora(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    //Funções para Retorno de valores da calculadora:
+
+    /**
+     * Método para retornar Soma
+     * @return
+     */
+    public double resultadoSomar() {
+        return (this.num1 + this.num2);
+    }
+    /**
+     * Método para retornar Subtração
+     * @return
+     */
+    public double resultadoSubtracao() {
+        return (this.num1 - this.num2);
+    }
+    /**
+     * Método para retornar Divisão
+     * @return
+     */
+    public double resultadoDivisao() {
+        return( this.num1 / this.num2);
+    }
+    /**
+     * Método para retornar Multiplicação
+     * @return
+     */
+    public double resultadoMultiplicacao() {
+        return (this.num1 * this.num2);
+    }
+
+}
