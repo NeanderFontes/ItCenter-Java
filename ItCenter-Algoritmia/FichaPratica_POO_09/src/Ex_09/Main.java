9c0697f5c34a56776f2111f9371447cc95c8b58c
Neander Fontes <neanderffontes@gmail.com>
2023-09-18 17:03:47 +0100

    Create Main.java
    Criando instancia do objeto Produto.java para criar sutuação de venda do produto de acordo com a quantidade em stock, inicializando quantidade em 0.

---
+++ ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_09/Main.java
@@ -0,0 +1,11 @@
+package Ex_09;
+
+public class Main {
+    public static void main(String[] args) {
+        //Instanciando objeto Produto:
+        Produto teste1 = new Produto("Neander", "Chocolate", 0.75);
+
+        //Chamada de menuProduto()
+        teste1.menuProduto();
+    }
+}
