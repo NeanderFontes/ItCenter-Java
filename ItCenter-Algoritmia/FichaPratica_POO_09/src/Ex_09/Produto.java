08ed625d917d1ba1310e80672333eaf410778958
Neander Fontes <neanderffontes@gmail.com>
2023-09-18 15:50:50 +0100

    Update Produto.java
    Incrementando código

--- ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_09/Produto.java
+++ ItCenter-Algoritmia/FichaPratica_POO_09/src/Ex_09/Produto.java
@@ -1,5 +1,148 @@
 package Ex_09;
 
-public class Produto {
+import java.util.Scanner;
 
+/**
+ * Crie uma classe Produto que tenha os atributos nome, preço e quantidade em stock
+    (deve ser inicializada a 0), e os métodos comprar e vender, que recebam a
+    quantidade desejada como parâmetro e atualizem o stock.
+    I - Para vender um produto, deve analisar se existe stock suficiente. Caso exista stock suficiente deve
+        atualizar o stock. Caso não exista stock suficiente deve informar o utilizador que tal transação não
+        é possível por quebra de stock do produto.
+    II - No método main, crie um objeto dessa classe Produto e chame o método para comprar 5 unidades.
+    III - Invoque o método para vender 2 unidades, e imprima a quantidade de stock.
+    IV - Invoque o método para comprar 10 unidades.
+    V - Invoque o método para vender 35 unidades
+ */
+public class Produto {
+    private String cliente ,nomePruduto;
+    private double precoProduto;
+    private int qtProduto = 0, qtStock = 0;
+
+    /**
+     * Método Construtor para obter Nome e Preço do Produto
+     * @param cliente - Nome do Cliente
+     * @param nomePruduto - Nome Produto
+     * @param precoProduto - Preço Produto
+     */
+    public Produto(String cliente, String nomePruduto, double precoProduto) {
+        this.cliente = cliente;
+        this.nomePruduto = nomePruduto;
+        this.precoProduto = precoProduto;
+    }
+
+    public void menuProduto() {
+        Scanner input = new Scanner(System.in);
+
+        int opcaoMenu;
+
+        do {
+            System.out.println("==== Mercado Sozinho ====");
+            System.out.println("1 - Comprar.");
+            System.out.println("2 - Stock.");
+            System.out.println("0 - Sair.");
+            opcaoMenu = input.nextInt();
+            switch (opcaoMenu) {
+                case 1:
+                    comprar();
+                    break;
+                case 2:
+                    atualizarEstoque();
+                    break;
+                case 0:
+                    System.out.println("Sair.");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        } while (opcaoMenu != 0);
+    }
+
+    public void comprar() {
+        //Entrada para quantidade de produtos;
+        Scanner input = new Scanner(System.in);
+
+        //Entrada e saída de dados:
+        System.out.println(getCliente() + "Seja bem vindo!");
+        System.out.println("Disponível " + getNomePruduto() + "[" + this.qtStock + "] no momento.");
+
+        //Se quantidade de stock maior que 0 intriduzir quantidade do produto
+        if (this.qtStock > 0) {
+            System.out.print("Quantos " + getNomePruduto() + " deseja comprar? ");
+            this.qtProduto = input.nextInt();
+            if (qtProduto > 0) {
+                this.precoProduto *= this.qtProduto;
+                System.out.println("Valor total = €" + this.precoProduto);
+                if (this.precoProduto > 0 && this.qtProduto > 0) {
+                    //Chamada da função para validar a venda:
+                    vender();
+                }
+            } else {
+                System.out.println("Obrigado, volte sempre!");
+            }
+        } else {
+            //Se não, quantidade de stock = 0
+            System.out.println("Infelizmente estamos sem stock de " +getNomePruduto());
+        }
+    }
+    public void vender() {
+        //SCanner para entrada de dados:
+        Scanner input = new Scanner(System.in);
+
+        //Declaração de variáveis locais:
+        String opcaoStock;
+        boolean atualizarStock = false;
+
+        //Se quantidade de stock = 0, atualizar stock
+        if (this.qtStock <= 0) {
+            System.out.println(getNomePruduto() + " Indisponível no momento.");
+            System.out.println("Deseja atualizar stock? ");
+            opcaoStock = input.next();
+            if (opcaoStock.equalsIgnoreCase("S")) {
+                atualizarStock = true;
+                if (atualizarStock) {
+                    System.out.print("Quantos produtos deseja adicionar ao Stock? ");
+                    int qtMaisStock = input.nextInt();
+                    atualizarEstoque(qtMaisStock);
+                }
+            } else {
+                System.out.println(getNomePruduto() + "Indisponível no momento.");
+            }
+        } else {
+            System.out.println("Qual a quantidade desejada do " + getNomePruduto() + " ?");
+            this.qtProduto = input.nextInt();
+            if (this.qtProduto > this.qtStock) {
+                System.out.println("Quantidade de produto pedido mairo que quantidade em stock.");
+                System.out.println("Disponível " + getNomePruduto() + "[" + this.qtStock + "] no momento.");
+            } else {
+                System.out.println("Disponível " + getNomePruduto() + "[" + this.qtStock + "] no momento.");
+            }
+        }
+    }
+
+    /**
+     * Função para retornar atribuição ao valor de Stock
+     * @param qtMaisStock
+     * @return
+     */
+    public int atualizarEstoque(int qtMaisStock) {
+        this.qtStock = qtMaisStock;
+            return this.qtStock ;
+    }
+
+    /**
+     * Método getter para nome Cliente
+     * @return - Nome do Cliente
+     */
+    public String getCliente() {
+        return this.cliente;
+    }
+
+    /**
+     * Método getter para nome do Produto
+     * @return - Nome do Produto
+     */
+    public String getNomePruduto() {
+        return this.nomePruduto;
+    }
 }
