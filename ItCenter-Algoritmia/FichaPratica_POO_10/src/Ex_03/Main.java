bbea7f045b067f66cd7611acd2e3ed71c27c27a5
Neander Fontes <neanderffontes@gmail.com>
2023-09-24 00:10:57 +0100

    Update Main.java
    Instanciando Objeto e comportamentos criados para realizar as ações desejadas.

--- ItCenter-Algoritmia/FichaPratica_POO_10/src/Ex_03/Main.java
+++ ItCenter-Algoritmia/FichaPratica_POO_10/src/Ex_03/Main.java
@@ -1,4 +1,25 @@
 package Ex_03;
 
+import Ex_04.AcabamentoImovel;
+import Ex_04.Imovel;
+import Ex_04.TipoApartamento;
+
 public class Main {
+    public static void main(String[] args) {
+        //Instânciando objeto Imovel:
+        Imovel aptoBraga = new Imovel(10,12,1,1,75.0, 0,"PT - Braga", AcabamentoImovel.USADO, TipoApartamento.APARTAMENTO);
+        Imovel aptoIgual = new Imovel(10,12,1,1,75.0, 0,"APT IGUAL", AcabamentoImovel.USADO, TipoApartamento.APARTAMENTO);
+        Imovel aptoImbui = new Imovel(214, 902,2,2,75.5,0,"BR - Salvador/BA", AcabamentoImovel.USADO, TipoApartamento.APARTAMENTO);
+        aptoBraga.descricaoImovel();
+        aptoBraga.valorImovel();
+        System.out.println("====================================");
+        System.out.println("\t\tVALOR DO IMOVEL");
+        aptoImbui.descricaoImovel();
+        aptoImbui.valorImovel();
+        System.out.println("=====================================");
+        System.out.println("\t\tCOMPARAÇÃO DE PREÇOS");
+        aptoBraga.comprarImovel(aptoImbui);
+        aptoImbui.comprarImovel(aptoBraga);
+        aptoBraga.comprarImovel(aptoIgual);
+    }
 }
