06030b5e400137aa67c44dbc49c3a57e4cf0ae72
Neander Fontes <neanderffontes@gmail.com>
2023-09-24 22:44:00 +0100

    Create Main.java
    Exemplo de teste: Instancie 5 pessoas (uma delas com menos de 18 anos). De seguida tente adicionar todas
    as 5 ao array de participantes. Imprima a lista de participantes e seguidamente imprima qual o vencedor

---
+++ ItCenter-Algoritmia/FichaPratica_POO_11/src/Ex_02/Main.java
@@ -0,0 +1,11 @@
+package Ex_02;
+
+/**
+ * Exemplo de teste: Instancie 5 pessoas (uma delas com menos de 18 anos). De seguida tente adicionar todas
+ * as 5 ao array de participantes. Imprima a lista de participantes e seguidamente imprima qual o vencedor
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
