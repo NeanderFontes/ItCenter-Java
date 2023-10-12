81d6e1af6487ac3412a6087e95a5b9fb35b758fa
Neander Fontes <neanderffontes@gmail.com>
2023-10-12 16:34:45 +0100

    Update TesteLuta.java
    Criando TesteLuta.java para avaliar código de lutas das subclasses criadas, Arqueiro ok!

--- ItCenter-Algoritmia/ProjetoFinal/src/RPG/Jogo/TesteLuta.java
+++ ItCenter-Algoritmia/ProjetoFinal/src/RPG/Jogo/TesteLuta.java
@@ -1,15 +1,32 @@
 package RPG.Jogo;
 
+import RPG.Entidades.NPC;
 import RPG.Entidades.Personagens.Aprendiz;
+import RPG.Entidades.Personagens.Arqueiro;
 import RPG.Entidades.Personagens.Gatuno;
+import RPG.Item.AbstractClass.ItemHeroi;
 import RPG.Item.ArmaPrincipal;
 
 public class TesteLuta {
     public static void main(String[] args) {
         ArmaPrincipal faca = new ArmaPrincipal("Faca de Iniciante", 0, 10, 12);
         Aprendiz novoAprendiz = new Aprendiz("Aprendiz Eterno", 50,10);
-        novoAprendiz.exibirDetalhes();
+        //novoAprendiz.exibirDetalhes();
         Gatuno assassinCred = new Gatuno("Assassin´s Cred", 100,100);
-        assassinCred.exibirDetalhes();
+        //assassinCred.exibirDetalhes();
+
+        ArmaPrincipal facaNPC = new ArmaPrincipal("Faca NPC", 10,12,12);
+        facaNPC.addHeroiPermitido("Arqueiro");
+
+        Arqueiro robinHood = new Arqueiro("Robin Hood", 100, 100);
+        NPC testeNPC = new NPC("Teste",400,10,10);
+        testeNPC.addInventarioNPC(facaNPC);
+
+        ItemHeroi arco = new ArmaPrincipal("Arco", 60, 60, 110);
+        arco.addHeroiPermitido("Arqueiro");
+        robinHood.setArmaPrincipalHeroi((ArmaPrincipal) arco);
+        robinHood.exibirDetalhes();
+        robinHood.atacar(testeNPC);
+        robinHood.exibirDetalhes();
     }
 }
