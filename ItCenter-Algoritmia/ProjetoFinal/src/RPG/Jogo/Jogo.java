169546bdc318e1b17d91f599c6057136f131855d
Neander Fontes <neanderffontes@gmail.com>
2023-10-13 18:42:38 +0100

    Update Jogo.java
    Atualizando Jogo.java para as necessidades real o package criado e Elabore uma Classe Jogo com um método criarPersonagem que permita criar uma personagem através de
    feedback da consola (pode criar métodos auxiliares nas respetivas classes).

--- ItCenter-Algoritmia/ProjetoFinal/src/RPG/Jogo/Jogo.java
+++ ItCenter-Algoritmia/ProjetoFinal/src/RPG/Jogo/Jogo.java
@@ -1,4 +1,20 @@
 package RPG.Jogo;
 
+import java.util.ArrayList;
+
 public class Jogo {
+    private String nomeJogo;
+    private int indexAtualSalaDoJogo;
+    private ArrayList<SalaDeJogo> salasJogo;
+
+    /**
+     * Método Construtor do Jogo:
+     *
+     * @param nomeJogo - Nome Para Criação do Jogo.
+     */
+    public Jogo(String nomeJogo) {
+        this.nomeJogo = nomeJogo;
+        this.indexAtualSalaDoJogo = 0;
+        this.salasJogo = new ArrayList<>();
+    }
 }
