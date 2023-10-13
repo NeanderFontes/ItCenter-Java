6c7d7a378d714b2fa6051cc647b572f47166c377
Neander Fontes <neanderffontes@gmail.com>
2023-10-13 18:41:16 +0100

    Update Mago.java
    Implementando código para Mago.java no método atacar e adaptando conforme sua SubClasse

--- ItCenter-Algoritmia/ProjetoFinal/src/RPG/Entidades/Personagens/Mago.java
+++ ItCenter-Algoritmia/ProjetoFinal/src/RPG/Entidades/Personagens/Mago.java
@@ -2,6 +2,10 @@ package RPG.Entidades.Personagens;
 
 import RPG.Entidades.Heroi;
 import RPG.Entidades.NPC;
+import RPG.Item.ConsumivelCombate;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Mago extends Heroi {
 
@@ -20,17 +24,155 @@ public class Mago extends Heroi {
      * Método @Override Adaptado ao Mago para Atacar um NPC
      * O Mago por não ter proteção devida recebe 10% de Dano a mais do ataque do NPC...
      * ... que por sua vez também tem 20% de dano a mais por ser Ataque Mágico
-     * Se o Mago ganhar a batalha: Nivel +1, +5% de HP e +2% de Força.
+     * Se o Mago ganhar a batalha: Nivel +1, +7% de HP e +2% de Força.
      *
      * @param oponenteNPC - Recebe como Parâmetro o Npc para Batalha
      */
     @Override
     public void atacar(NPC oponenteNPC) {
+        //Scaner para Entrada de dados:
+        Scanner input = new Scanner(System.in);
 
+        //Declaração de variáveis do escopo:
+        int qtRoud = 1;
+        int hpNPC = oponenteNPC.getVidaEntidade();
+        int hpPersonagem = this.getVidaEntidade();
+        int opcaoAtaqueHeroi;
+        //Criando variável para aumentar 10% do Ataque do Oponente:
+        int aumentarAtaqueOponenteDezPorcento = (oponenteNPC.getForcaEntidade() * 10) / 100;
+        //Criando variável para aumentar 20% do Ataque Mágico do Herói Mago:
+        int aumentarAtaqueMagicoMago = (getForcaEntidade() * 20) / 100;
+
+        //Prioridade de ataque do Mago:
+        System.err.println("\t\t*** Prioridade de Ataque de " + this.getNomeEntidade() + " ***");
+
+        do {
+            //Simulação de ataque do Mago:
+            System.out.println("======================= ROUND " + qtRoud++ + " ========================");
+            System.out.println("[ 1 ]   -   Ataque Normal");
+            System.out.println("[ 2 ]   -   Ataque Especial");
+            System.out.println("[ 3 ]   -   Usar Consumível de Ataque");
+            System.out.println("========================================================");
+            System.out.print(this.getNomeEntidade() + "!! Escolha uma Opção de Atk acima: ");
+
+            //Tratamento de Erro para "InputMismatchException" entrada de dados diferente de números:
+            try {
+                opcaoAtaqueHeroi = input.nextInt();
+            } catch (InputMismatchException mismatchException) {
+                System.err.println("Erro: Entrada inválida. Certifique-se de digitar um número inteiro.");
+                input.next();
+                opcaoAtaqueHeroi = 0;
+            }
+
+            switch (opcaoAtaqueHeroi) {
+                case 1: //Ataque Normal do Mago:
+                    System.out.println("\n" + this.getNomeEntidade() + " Usa a(o) " + this.getArmaPrincipalHeroi().getNomeItemHeroi() + " e.......");
+                    System.out.println("»-----> \n\t\t\t»-----> \n\t\t\t\t\t\t»-----> \n\t\t\t\t\t\t\t\t\t»--- OUCH!");
+                    System.out.println("\t*** ATAQUE REALIZADO COM SUCESSO!! ***");
+                    System.out.println("\t\t\t\tDANO = " + this.getArmaPrincipalHeroi().getAtaqueNormal() + " ATK\n");
+                    hpNPC -= this.getForcaEntidade() + this.getArmaPrincipalHeroi().getAtaqueNormal() + aumentarAtaqueMagicoMago;
+                    if (hpNPC > 0) {
+                        //Utilização do setter:
+                        //npcGamer.setVidaEntidade(npcGamer.getVidaEntidade()-this.getForcaEntidade());
+                        System.out.println("Hp Atual do Oponente " + oponenteNPC.getNomeEntidade() + " = " + hpNPC + " hp.");
+                    } else if (hpNPC <= 0) {
+                        System.out.println("Hp do Oponente " + oponenteNPC.getNomeEntidade() + " = 0");
+                    }
+                    break;
+                case 2: //Ataque Especial do Mago:
+                    System.err.println("\t\t\t   *** " + this.getNomeEntidade() + " USA ***");
+                    System.err.println("\t   *** ATAQUE ESPECIAL ATAQUE ESPIRITUAL ***");
+                    System.out.println("\n");
+                    System.out.println("  _-==---_\t       _-==---_\n" +
+                            " -===  _  -\t      -===  _  -\n" +
+                            "-==== ( )  -\t     -==== ( )  -\n" +
+                            "-=====     -\t     -=====     -\n" +
+                            "-==( )=    -\t     -==( )=    -\n" +
+                            " -=====  _-\t      -=====  _-\n" +
+                            "   -==---     \t\t-==--- ");
+                    System.out.println("\n\t*** ATAQUE ESPECIAL REALIZADO COM SUCESSO!! ***");
+                    System.out.println("\t\t\t\tDANO = " + this.getArmaPrincipalHeroi().getAtaqueNormal() + " ATK\n");
+                    hpNPC -= this.getForcaEntidade() + this.getArmaPrincipalHeroi().getAtaqueEspecial() + aumentarAtaqueMagicoMago;
+                    if (hpNPC >= 0) {
+                        //Utilização do setter:
+                        //npcGamer.setVidaEntidade(npcGamer.getVidaEntidade()-this.getForcaEntidade());
+                        System.out.println("Hp Atual do Oponente " + oponenteNPC.getNomeEntidade() + " = " + hpNPC + " hp.");
+                    }
+                    break;
+                case 3: //Consumível de Ataque:
+                    ConsumivelCombate consumivelUsado = heroiUsarComsumivelCombate();
+                    if (consumivelUsado != null) {
+                        //Remoção do Consumível de Combate Usado.
+                        this.inventarioHeroi.remove(consumivelUsado);
+
+                        //Ataque do Consumivel de Combate ao NPC:
+                        hpNPC -= consumivelUsado.getAtaqueInstataneo();
+                    } else {//Não existe Consumivel de combate e retorna para nova opção:
+                        opcaoAtaqueHeroi = 0;
+                    }
+                    break;
+                default: //Opção Inválida
+                    System.err.println("\n\t\t\t*** Opção Inválida ***\n");
+                    break;
+            }
+
+            //Simulação de ataque do NPC:
+            if (opcaoAtaqueHeroi >= 1 && opcaoAtaqueHeroi <= 3) {
+                System.out.println("\n" + oponenteNPC.getNomeEntidade() + " Ataca!!! e.......");
+                System.out.println("OUCH!-----« \n\t\t\t<-----« \n\t\t\t\t\t\t<-----« \n\t\t\t\t\t\t\t\t\t<-----«");
+                System.out.println("\t*** ATAQUE REALIZADO COM SUCESSO!! ***");
+                System.out.println("\t\t\t\tDANO = " + oponenteNPC.getForcaEntidade() + " ATK\n");
+                hpPersonagem -= oponenteNPC.getForcaEntidade() + aumentarAtaqueOponenteDezPorcento;
+                if (hpNPC <= 0) {
+                    hpPersonagem += oponenteNPC.getForcaEntidade() - aumentarAtaqueOponenteDezPorcento;
+                }
+                if (hpPersonagem > 0) {
+                    System.out.println("Hp Atual do Herói " + this.getNomeEntidade() + " = " + hpPersonagem + " hp.");
+                } else if (hpPersonagem <= 0) {
+                    System.out.println("O Herói Morreu");
+                }
+            } else {
+                qtRoud--;
+            }
+
+            //Laço executável até a Quantidade de HP de algum dos oponentes acabar.
+        } while (hpNPC > 0 && hpPersonagem > 0);
+
+        //Dados finais da Batalha:
+        if (hpPersonagem <= 0) { //Perder Batalha
+            System.out.println(this.getNomeEntidade() + "Infelizmente Perdeu a Luta");
+
+            //Teste para resultados:
+            System.out.println("Teste de Resultados: ");
+            System.out.println("Nivel do Heroi: " + this.getNivelHeroi());
+            System.out.println("For = " + this.getForcaEntidade());
+            //TODO HP FICANDO NEGATIVO AO FINAL DA DERROTA
+            System.out.println("HP = " + hpPersonagem);
+            System.out.println("Ouro = " + this.getOuroHeroi());
+
+        } else { //Ganhar Batalha
+            System.out.println(this.getNomeEntidade() + " É o Vencedor!!!");
+            //Aumenta Nivel + 1:
+            this.setNivelHeroi(getNivelHeroi() + 1);
+
+            //Aumenta HP em 7%:
+            int aumentoVida = (this.getVidaEntidade() * 7) / 100;
+            this.setVidaEntidade(this.getVidaEntidade() + aumentoVida);
+
+            //AUmenta Força em 2%:
+            int aumentoForca = (this.getForcaEntidade() * 2 / 100);
+            this.setForcaEntidade(this.getForcaEntidade() + aumentoForca);
+
+            //Chamda de função da SuperClass<b>Heroi<b>
+            this.heroiRecolherItemNPC(oponenteNPC);
+
+            //Teste para resultados:
+            System.out.println("Teste de Resultados: ");
+            System.out.println("Nivel do Heroi: " + this.getNivelHeroi());
+            System.out.println("For = " + this.getForcaEntidade());
+            System.out.println("HP = " + hpPersonagem);
+            System.out.println("Ouro = " + this.getOuroHeroi());
+            System.out.println("Arma Principal: " + this.getArmaPrincipalHeroi().getNomeItemHeroi());
+        }
     }
-
-    /**
-     * Método Getter e Setter do Mago
-     */
-
 }
