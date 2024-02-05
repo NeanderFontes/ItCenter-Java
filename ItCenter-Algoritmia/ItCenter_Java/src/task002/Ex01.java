9071b9344d70bb1ae44eadf3d37e064a84e8cf48
Neander Fontes <neanderffontes@gmail.com>
2024-02-05 22:02:08 +0000

    TASK-002

--- ItCenter-Algoritmia/ItCenter_Java/src/task002/Ex01.java
+++ ItCenter-Algoritmia/ItCenter_Java/src/task002/Ex01.java
@@ -1,2 +1,42 @@
-package task002;public class Ex01 {
+package task002;
+
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        /**
+         * 1. Leia dois números inteiros e escreva na consola o maior deles.
+         * Exemplo de execução:
+         * - Introduza um numero: 40
+         * - Introduza um numero: 5
+         * - Maior: 40
+         */
+
+        // Scanner input for data
+        Scanner input = new Scanner(System.in);
+
+        // Declaration of variables
+        String resultadoValor = "";
+        int num1, num2;
+
+        // Data input by the user
+        System.out.println("Enter two values to find the largest between them");
+        System.out.print("First value: ");
+        num1 = input.nextInt();
+        System.out.print("Second value: ");
+        num2 = input.nextInt();
+
+        if (num1 == num2) {
+            resultadoValor = "Both numbers are equal!";
+        } else if (num1 < num2) {
+            resultadoValor = (" Is the lowest value.");
+        } else if (num1 > num2) {
+            resultadoValor = (" Is the highest value");
+        } else {
+            resultadoValor = "All the value is 0";
+        }
+
+        // Final data
+        System.out.println(resultadoValor);
+    }
 }
